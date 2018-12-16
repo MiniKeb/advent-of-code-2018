908fad80e42eb8e5806282c530f80eea193f0066
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-12-16 15:08:38 +0100

    Ajoute le calcul de parcours

--- day-07/steps.test.js
+++ day-07/steps.test.js
@@ -1,4 +1,14 @@
 describe("steps", () => {
+  const exercice = [
+    "Step C must be finished before step A can begin.",
+    "Step C must be finished before step F can begin.",
+    "Step A must be finished before step B can begin.",
+    "Step A must be finished before step D can begin.",
+    "Step B must be finished before step E can begin.",
+    "Step D must be finished before step E can begin.",
+    "Step F must be finished before step E can begin."
+  ];
+
   it("extrait un step d'apès une instruction", () => {
     const instruction = "Step C must be finished before step A can begin.";
 
@@ -9,17 +19,7 @@ describe("steps", () => {
   });
 
   it("crée une structure de steps d'après plusieurs instructions", () => {
-    const instructions = [
-      "Step C must be finished before step A can begin.",
-      "Step C must be finished before step F can begin.",
-      "Step A must be finished before step B can begin.",
-      "Step A must be finished before step D can begin.",
-      "Step B must be finished before step E can begin.",
-      "Step D must be finished before step E can begin.",
-      "Step F must be finished before step E can begin."
-    ];
-
-    expect(structure(instructions)).toEqual({
+    expect(structure(exercice)).toEqual({
       ["C"]: { precedents: [] },
       ["A"]: { precedents: ["C"] },
       ["F"]: { precedents: ["C"] },
@@ -29,22 +29,22 @@ describe("steps", () => {
     });
   });
 
-  it("prend comme prochaine instruction celle sans précédents", () => {
+  it("prend comme prochain step celui sans précédents", () => {
     const unSansPrecedent = {
       C: { precedents: [] },
       A: { precedents: ["C"] }
     };
 
-    expect(instruction(unSansPrecedent)).toBe("C");
+    expect(nextStep(unSansPrecedent)).toBe("C");
   });
 
-  it("prend la première lettre quand plusieurs instructions sont disponibles", () => {
-    const structure = {
+  it("prend la première lettre quand plusieurs steps sont disponibles", () => {
+    const avecEgalite = {
       C: { precedents: [] },
       A: { precedents: [] }
     };
 
-    expect(instruction(structure)).toBe("A");
+    expect(nextStep(avecEgalite)).toBe("A");
   });
 
   it("nettoie une structure", () => {
@@ -57,7 +57,23 @@ describe("steps", () => {
       A: { precedents: [] }
     });
   });
+
+  it("calcule un parcours", () => {
+    expect(parcours(exercice)).toBe("CABDFE");
+  });
 });
+
+function parcours(instructions) {
+  let parcours = "";
+  let trajet = structure(instructions);
+  let next = nextStep(trajet);
+  while (next) {
+    parcours += next;
+    trajet = nettoyer(trajet, next);
+    next = nextStep(trajet);
+  }
+  return parcours;
+}
 
 function nettoyer(structure, cible) {
   const precedentsNettoyes = Object.entries(structure).reduce(
@@ -83,10 +99,12 @@ function nettoyer(structure, cible) {
   );
 }
 
-function instruction(structure) {
+function nextStep(structure) {
   const disponibles = Object.entries(structure).filter(
     ([key, { precedents }]) => precedents.length === 0
   );
+
+  if (disponibles.length === 0) return null;
 
   let choisie = disponibles[0][0];
   if (disponibles.length > 0) {
